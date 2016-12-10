28952cc853707cb6f8000e7f7e62e7a7eed521d4
Sagher Mehmood <sagherhundal@gmail.com>
2016-12-10 11:48:21 +0500

    Optimized and minified html

---
+++ src/main/resources/static/js/loadTemplates.js
@@ -0,0 +1,11 @@
+$(function() {
+	$("#navbar-logo").load("/html-templates/navbarLogo.html");
+
+	$("#modal").load("/html-templates/modal.html");
+	
+	$("#up").load("/html-templates/up.html");
+	
+	$("#nav-left").load("/html-templates/nav-left.html");
+	
+	$("#sidebar").load("/html-templates/sidebar.html");
+});
